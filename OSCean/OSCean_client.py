4fcc27d904ff59ca80e4e335a5da3997d0a9c188
Daniel Rosero <dannrosero@gmail.com>
2021-10-15 20:41:39 +0100

    Underwater temp + Light temp

--- OSCean/OSCean_client.py
+++ OSCean/OSCean_client.py
@@ -9,8 +9,9 @@ config = {
 }
 
 def stream_handler(message):
-    print(message["event"]) # put
-    print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+    # print(message["event"]) # put
+    # print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+    # print(message["temp_underwater"]) # {'title': 'Pyrebase', "body": "etc..."}
     print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
 
     # if message["data"] == 1:
@@ -23,6 +24,22 @@ def stream_handler(message):
 
     client.send_message("/ocean2osc/aveiro/temperature",message["data"])
 
+def stream_handler_light(message):
+    # print(message["event"]) # put
+    # print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+    # print(message["light"]) # {'title': 'Pyrebase', "body": "etc..."}
+
+    # if message["data"] == 1:
+    # 	print ("Sending OSC to show content")
+    # 	client.send_message("/Content/test/enabled", 1)
+
+    # if message["data"] == 0:
+    # 	print ("Sending OSC to hide content")
+    # 	client.send_message("/Content/test/enabled", 0)
+
+    client.send_message("/ocean2osc/aveiro/light",message["data"])
+
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
@@ -31,7 +48,9 @@ client = udp_client.SimpleUDPClient("127.0.0.1", 8000)
 
 
 
-my_stream = db.child("aveiro/temperature/data").stream(stream_handler)
+my_stream = db.child("aveiro/temp_underwater/").stream(stream_handler)
+my_streamlight = db.child("aveiro/light/").stream(stream_handler_light)
+
 
 
 
